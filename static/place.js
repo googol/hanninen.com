ba979af77887f5b6da9c04e6026e46596bebacc0
Miika Hänninen <miika.hanninen@relexsolutions.com>
2023-07-21 21:12:18 +0300

    Add place.js

---
+++ static/place.js
@@ -0,0 +1,7 @@
+const embed = document.querySelector('garlic-bread-embed')
+
+const canvas = document.querySelector('garlic-bread-embed').shadowRoot.querySelector('garlic-bread-share-container').querySelector('garlic-bread-canvas').shadowRoot.querySelector('canvas')
+const statusPill = document.querySelector('garlic-bread-embed').shadowRoot.querySelector('garlic-bread-share-container').querySelector('garlic-bread-status-pill')
+const colorPicker = document.querySelector('garlic-bread-embed').shadowRoot.querySelector('garlic-bread-share-container').querySelector('garlic-bread-color-picker')
+
+console.log(embed, canvas, statusPill, colorPicker)
